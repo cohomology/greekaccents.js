5b25148f7bbb403a83c5cc900c218aa82afc3555
Kilian Kilger <kkilger@gmail.com>
2019-06-21 20:51:48 +0200

    Enable tslint

--- lib/accents.ts
+++ lib/accents.ts
@@ -1,13 +1,13 @@
 class GreekAccents {
-  public akut : boolean = false;
-  public gravis : boolean = false;
-  public spiritus_asper : boolean = false;
-  public spiritus_lenis : boolean = false; 
-  public circumflex : boolean = false;
-  public iota_subscriptum : boolean = false;
-  public dialytika : boolean = false;
-  public breve : boolean = false;  
-  public makron : boolean = false;   
-  public tonos : boolean = false;    
-  public end_of_word : boolean = false;     
+  public akut: boolean = false;
+  public gravis: boolean = false;
+  public spiritusAsper: boolean = false;
+  public spiritusLenis: boolean = false;
+  public circumflex: boolean = false;
+  public iotaSubscriptum: boolean = false;
+  public dialytika: boolean = false;
+  public breve: boolean = false;
+  public makron: boolean = false;
+  public tonos: boolean = false;
+  public endOfWord: boolean = false;
 }
