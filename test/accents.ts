e2595fd1ac734942fb63e9689a9b2a5e049926b8
Kilian Kilger <kkilger@gmail.com>
2019-06-30 19:07:56 +0200

    Implement basic constructor test

--- test/accents.ts
+++ test/accents.ts
@@ -4,10 +4,30 @@ import { IGreekAccents, GreekAccents } from '../lib';
 describe('GreekAccentsTest', () => {
   it('can be constructed', () => {
     let accent = new GreekAccents();
-    for (let attr in accent) {
-      expect(typeof attr === 'boolean');
+    for(let prop in accent) {
+      let value = Reflect.get(accent, prop);
+      expect(typeof(value)).eql('boolean');
+      expect(value).eql(false);
     }
   });
+  it('check constructor', () => {
+    const proto = new GreekAccents();
+    let propertyArray : string[] = [];
+    Object.keys(proto).forEach( key => {
+      propertyArray.push(key);
+    });
+    propertyArray.forEach((item,index) => {
+      let hasExactlyOneProperty = {};
+      Reflect.set(hasExactlyOneProperty, item, true);
+      let hasProp : IGreekAccents = <any>(hasExactlyOneProperty);
+      let accent = new GreekAccents(hasProp);
+      for(let prop in accent) {
+        let value = Reflect.get(accent, prop);
+        expect(typeof(value)).eql('boolean');
+        expect(value).eql(item === prop);
+      } 
+    });
+  }); 
 })
 
 
