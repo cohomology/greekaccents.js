3cef88babf23e45e1b8406f1c6a90ec5a3473970
Kilian Kilger <kkilger@gmail.com>
2019-07-03 21:38:37 +0200

    Some more bit-fiddling

--- lib/accents.ts
+++ lib/accents.ts
@@ -1,17 +1,17 @@
 /* tslint:disable:no-bitwise */
 
 export enum EGreekAccent {
-  Akut            = 1 << 1,
-  Gravis          = 1 << 2,
-  SpiritusAsper   = 1 << 3,
-  SpiritusLenis   = 1 << 4,
-  Circumflex      = 1 << 5,
-  IotaSubscriptum = 1 << 6,
-  Dialytika       = 1 << 7,
-  Breve           = 1 << 8,
-  Makron          = 1 << 9,
-  Tonos           = 1 << 10,
-  EndOfWord       = 1 << 11,
+  Akut            = 1,
+  Gravis          = 2,
+  SpiritusAsper   = 3,
+  SpiritusLenis   = 4,
+  Circumflex      = 5,
+  IotaSubscriptum = 6,
+  Dialytika       = 7,
+  Breve           = 8,
+  Makron          = 9,
+  Tonos           = 10,
+  EndOfWord       = 11,
 }
 
 export interface IGreekAccents {
@@ -34,30 +34,50 @@ export class GreekAccents {
   constructor(accents?: IGreekAccents) {
     this.accents = 0;
     if (accents) {
-      if (accents.akut) { this.accents += EGreekAccent.Akut; }
-      if (accents.gravis) { this.accents += EGreekAccent.Gravis; }
-      if (accents.spiritusAsper) { this.accents += EGreekAccent.SpiritusAsper; }
-      if (accents.spiritusLenis) { this.accents += EGreekAccent.SpiritusLenis; }
-      if (accents.circumflex) { this.accents += EGreekAccent.Circumflex; }
-      if (accents.iotaSubscriptum) { this.accents += EGreekAccent.IotaSubscriptum; }
-      if (accents.dialytika) { this.accents += EGreekAccent.Dialytika; }
-      if (accents.breve) { this.accents += EGreekAccent.Breve; }
-      if (accents.makron) { this.accents += EGreekAccent.Makron; }
-      if (accents.tonos) { this.accents += EGreekAccent.Tonos; }
-      if (accents.endOfWord) { this.accents += EGreekAccent.EndOfWord; }
+      if (accents.akut) { this.accents += 1 << EGreekAccent.Akut; }
+      if (accents.gravis) { this.accents += 1 << EGreekAccent.Gravis; }
+      if (accents.spiritusAsper) { this.accents += 1 << EGreekAccent.SpiritusAsper; }
+      if (accents.spiritusLenis) { this.accents += 1 << EGreekAccent.SpiritusLenis; }
+      if (accents.circumflex) { this.accents += 1 << EGreekAccent.Circumflex; }
+      if (accents.iotaSubscriptum) { this.accents += 1 << EGreekAccent.IotaSubscriptum; }
+      if (accents.dialytika) { this.accents += 1 << EGreekAccent.Dialytika; }
+      if (accents.breve) { this.accents += 1 << EGreekAccent.Breve; }
+      if (accents.makron) { this.accents += 1 << EGreekAccent.Makron; }
+      if (accents.tonos) { this.accents += 1 << EGreekAccent.Tonos; }
+      if (accents.endOfWord) { this.accents += 1 << EGreekAccent.EndOfWord; }
    }
   }
 
-  public akut(): boolean { return (this.accents & EGreekAccent.Akut) !== 0; }
-  public gravis(): boolean { return (this.accents & EGreekAccent.Gravis) !== 0; }
-  public spiritusAsper(): boolean { return (this.accents & EGreekAccent.SpiritusAsper) !== 0; }
-  public spiritusLenis(): boolean { return (this.accents & EGreekAccent.SpiritusLenis) !== 0; }
-  public circumflex(): boolean { return (this.accents & EGreekAccent.Circumflex) !== 0; }
-  public iotaSubscriptum(): boolean { return (this.accents & EGreekAccent.IotaSubscriptum) !== 0; }
-  public dialytika(): boolean { return (this.accents & EGreekAccent.Dialytika) !== 0; }
-  public breve(): boolean { return (this.accents & EGreekAccent.Breve) !== 0; }
-  public makron(): boolean { return (this.accents & EGreekAccent.Makron) !== 0; }
-  public tonos(): boolean { return (this.accents & EGreekAccent.Tonos) !== 0; }
+  public akut(): boolean { return (this.accents & (1 << EGreekAccent.Akut)) !== 0; }
+  public gravis(): boolean { return (this.accents & (1 << EGreekAccent.Gravis)) !== 0; }
+  public spiritusAsper(): boolean { return (this.accents & (1 << EGreekAccent.SpiritusAsper)) !== 0; }
+  public spiritusLenis(): boolean { return (this.accents & (1 << EGreekAccent.SpiritusLenis)) !== 0; }
+  public circumflex(): boolean { return (this.accents & (1 << EGreekAccent.Circumflex)) !== 0; }
+  public iotaSubscriptum(): boolean { return (this.accents & (1 << EGreekAccent.IotaSubscriptum)) !== 0; }
+  public dialytika(): boolean { return (this.accents & (1 << EGreekAccent.Dialytika)) !== 0; }
+  public breve(): boolean { return (this.accents & (1 << EGreekAccent.Breve)) !== 0; }
+  public makron(): boolean { return (this.accents & (1 << EGreekAccent.Makron)) !== 0; }
+  public tonos(): boolean { return (this.accents & (1 << EGreekAccent.Tonos)) !== 0; }
+  public endOfWord(): boolean { return (this.accents & (1 << EGreekAccent.EndOfWord)) !== 0; }
+
+  public setAkut(value: boolean) { this._updateBit(EGreekAccent.Akut, value); }
+  public setGravis(value: boolean) { this._updateBit(EGreekAccent.Gravis, value); }
+  public setSpiritusAsper(value: boolean) { this._updateBit(EGreekAccent.SpiritusAsper, value); }
+  public setSpiritusLenis(value: boolean) { this._updateBit(EGreekAccent.SpiritusLenis, value); }
+  public setCircumflex(value: boolean) { this._updateBit(EGreekAccent.Circumflex, value); }
+  public setIotaSubscriptum(value: boolean) { this._updateBit(EGreekAccent.IotaSubscriptum, value); }
+  public setDialytika(value: boolean) { this._updateBit(EGreekAccent.Dialytika, value); }
+  public setBreve(value: boolean) { this._updateBit(EGreekAccent.Breve, value); }
+  public setMakron(value: boolean) { this._updateBit(EGreekAccent.Makron, value); }
+  public setTonos(value: boolean) { this._updateBit(EGreekAccent.Tonos, value); }
+  public setEndOfWord(value: boolean) { this._updateBit(EGreekAccent.EndOfWord, value); }
+
   /** @internal */
   public _internalRepresentation(): number { return this.accents; }
+  /** @internal */
+  public _updateBit(bitPosition: number, bitValue: boolean) {
+    const bitValueNormalized = bitValue ? 1 : 0;
+    const clearMask = ~(1 << bitPosition);
+    this.accents = (this.accents & clearMask) | (bitValueNormalized << bitPosition);
+  }
 }
--- test/accents.ts
+++ test/accents.ts
@@ -1,3 +1,5 @@
+/* tslint:disable:no-bitwise */
+
 import { expect } from "chai";
 import { GreekAccents, IGreekAccents } from "../lib";
 
@@ -5,7 +7,7 @@ describe("GreekAccentsTest", () => {
   it("can be constructed", () => {
     const accent = new GreekAccents();
     Object.getOwnPropertyNames(GreekAccents.prototype).forEach( (name) => {
-      if (name !== "constructor" && name.charAt(0) !== "_") {
+      if (name !== "constructor" && name.charAt(0) !== "_" && name.substring(0, 3) !== "set") {
         const value = (accent as any)[name]();
         expect(typeof(value)).eql("boolean");
         expect(value).eql(false);
@@ -13,18 +15,24 @@ describe("GreekAccentsTest", () => {
     });
     expect(accent._internalRepresentation()).eql(0);
   });
-  it("check constructor", () => {
+  it("check constructor/set/get", () => {
     const propertyArray: string[] = [];
-    Object.getOwnPropertyNames(GreekAccents.prototype).forEach( (name) => {
-      if (name !== "constructor" && name.charAt(0) !== "_") {
+    Object.getOwnPropertyNames(GreekAccents.prototype).forEach( (name, index) => {
+      if (name !== "constructor" && name.charAt(0) !== "_" && name.substring(0, 3) !== "set") {
         propertyArray.push(name);
       }
     });
-    propertyArray.forEach( (name) => {
+    propertyArray.forEach( (name, index) => {
       const hasExactlyOneProperty = {};
       Reflect.set(hasExactlyOneProperty, name, true);
-      const hasProp: IGreekAccents = (hasExactlyOneProperty) as any;
+      const hasProp: IGreekAccents = hasExactlyOneProperty as any;
       const accent = new GreekAccents(hasProp);
+      expect(accent._internalRepresentation()).eql(1 << (index + 1));
+      const otherAccent = new GreekAccents();
+      (otherAccent as any)["set" + name.substring(0, 1).toUpperCase() + name.substring(1)](true);
+      expect(otherAccent._internalRepresentation()).eql(1 << (index + 1));
+      (otherAccent as any)["set" + name.substring(0, 1).toUpperCase() + name.substring(1)](false);
+      expect(otherAccent._internalRepresentation()).eql(0);
       propertyArray.forEach( (toBeTestedName) => {
         const value = (accent as any)[toBeTestedName]();
         expect(typeof(value)).eql("boolean");
